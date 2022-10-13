795c88acc0e06aee273a41ea92e75cfeff52562c
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2022-10-13 17:58:59 +0900

    Day2 complete

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from users import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path("signup/", views.signup, name="signup"),
+    path("login/", views.login, name="login"),
+    path("user/", views.user, name="user"),
+]
---
+++ users/views.py
@@ -0,0 +1,48 @@
+from http import HTTPStatus
+from urllib import response
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import User
+from django.contrib.auth import authenticate, login as loginsession
+
+# Create your views here.
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html')
+    elif request.method =='POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        passwordcheck = request.POST.get('passwordcheck', None)
+
+        if password != passwordcheck:
+            return render(request, 'user/signup.html')
+        else:
+            #에러처리
+            User.objects.create_user(username=username, password=password)
+            # 안되는 코드
+            # user = User()
+            # user.password = password
+            # return redirect('/sign-in') # 회원가입이 완료되었으므로 로그인 페이지로 이동
+            return render(request, 'login.html')
+            return HttpResponse("Good")
+    else:
+        # 안좋은 코드
+        return HttpResponse("Bad Method")
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method =='POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            print(user)
+            loginsession(request, user)
+            return redirect('users:user')
+        else:
+            return HttpResponse("로그인 실패")
+
+def user(request):
+    return HttpResponse(request.user)
