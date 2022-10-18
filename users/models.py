1f85fed7e01babfdb7c46d36995344e0ab6f2e5e
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2022-10-18 20:19:59 +0900

    add : comment model

--- users/models.py
+++ users/models.py
@@ -5,3 +5,4 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     profile = models.TextField(max_length=500, blank=True)
+    nickname = models.CharField(max_length=20, blank=True)
